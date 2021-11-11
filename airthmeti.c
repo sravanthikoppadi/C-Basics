0fdb3c9951d7b54fd8fe3b56a31cd1819ca30247
sravanthikoppadi <94115385+sravanthikoppadi@users.noreply.github.com>
2021-11-11 17:22:35 +0530

    Add files via upload

---
+++ airthmeti.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);//
+	printf("addition of %d and %d is: %d\n",a,b,a+b);
+	printf("substraction of %d and %d is: %d\n",a,b,a-b)	;
+	printf("multiplication of %d and %d is %d\n",a,b,a*b);
+	printf("div of %d and %d is %d\n",a,b,a/b);
+	printf("mod of %d and %d is %d",a,b,a%b);
+	}
